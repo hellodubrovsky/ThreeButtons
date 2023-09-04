6b4f0000da58d219c83c4b8f70694e58c8d15c21
Дубровский Илья <hellodubrovsky@icloud.com>
2023-09-04 22:05:22 +0300

    [2] It's start.

--- ThreeButtons/ThreeButtons/ViewController.swift
+++ ThreeButtons/ThreeButtons/ViewController.swift
@@ -9,8 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private lazy var firstButton: UIButton = {
+        let button = UIButton(frame: .zero)
+        button.backgroundColor = .blue
+        button.layer.cornerRadius = 8.0
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
+        self.view.addSubview(firstButton)
+        setConstraints()
+        setButton()
+    }
+
+    func setButton() {
+        self.firstButton.setTitle("Hello", for: .normal)
+        self.firstButton.setImage(.init(systemName: "arrow.right.circle.fill"), for: .normal)
+    }
+
+    func setConstraints() {
+        self.firstButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        self.firstButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
     }
 }
